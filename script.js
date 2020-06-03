9e63aa503cdb94e969c818beee45cd3ecb0d7cb3
EthanVB123 <ethanvanbruchem@gmail.com>
2020-06-03 08:39:52 +0800

    v3 map working with player, starting on keybinds

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ var dexterity = 1;
 var magic = 1;
 var gold = 10;
 
+var playerx = 5;
+var playery = 5;
+
 // Weapons are [name, MP, damage]
 var weapon1 = ['Your fists', 1, strength]
 var weapon2 = ['[Empty]', 0, 0]
@@ -36,6 +39,7 @@ var roomsExplored = [0,0,0,0,0]
 // 1 = wall
 // 2 = passage
 // 3 = green slime
+// 'p' = player
 /* 
 Room structure
 0000000000
@@ -49,11 +53,21 @@ Room structure
 0111111110
 0000000000
 */
-var room1fullstring = '0000000000011111111001000000100100000010010003001001000000100100000010010000001001111111100000000000'
-var room1splitstring = room1fullstring
+var room1fullstringWithoutPlayer = '0000000000011111111001000000100100000010010003001001000000100100000010010000001001111111100000000000'
+var room1splitstring = room1fullstringWithoutPlayer
+var room1fullstringWithPlayer = room1fullstringWithoutPlayer
 // Event listeners
     setInterval(mainGameLoop, 100)
+    document.addEventListener('keydown', keyPressed())
 // Functions
+// key Pressed function - what to do if a key is pressed?
+function keyPressed() {
+    console.log('Key is pressed!')
+    if (event.key == 39) {
+        playerx += 1
+    }
+
+}
 // change HP: use positive number to heal, use negative number to take damage
 function changeHP(amount) {
     currentHP += amount
@@ -62,9 +76,13 @@ function changeHP(amount) {
     }
 }
 // updateMap: the room variable is a 10x10 array variable
-
+function addPlayerToMap() {
+    playerIndex = 10*playery + playerx
+    room1fullstringWithPlayer = room1fullstringWithoutPlayer.substring(0,playerIndex)+'p'+room1fullstringWithoutPlayer.substring(playerIndex+1)
+    console.log(room1fullstringWithPlayer)
+}
 function updateMap() {
-    room1splitstring = room1fullstring
+    room1splitstring = room1fullstringWithPlayer
     var square = '00'
     var square1 = 0
     var square2 = 0
@@ -74,10 +92,10 @@ function updateMap() {
         } else {
             square = i.toString()
         }
-        console.log(square)
+        
         if (room1splitstring[0] == 0) {
-            console.log('Statement called')
-            document.getElementsByClassName(square)[0].src = 'PixelDungeonImages/entirelyWhiteSquare.png'
+            
+            document.getElementsByClassName(square)[0].src = 'PixelDungeonImages/UI/Map/Floor.png'
             room1splitstring = room1splitstring.substring(1)
 
         } else if (room1splitstring[0] == 1) {
@@ -86,6 +104,9 @@ function updateMap() {
         } else if (room1splitstring[0] == 3) {
             document.getElementsByClassName(square)[0].src = 'PixelDungeonImages/Creatures/GreenSlime.png'
             room1splitstring = room1splitstring.substring(1)
+        } else if (room1splitstring[0] == 'p') {
+            document.getElementsByClassName(square)[0].src = 'PixelDungeonImages/Creatures/player.png'
+            room1splitstring = room1splitstring.substring(1)
         }
 
     
@@ -93,6 +114,7 @@ function updateMap() {
     }
 }
 function mainGameLoop() {
+    addPlayerToMap()
     updateMap()
-    console.log('Main game loop activated!')
+    
 }
