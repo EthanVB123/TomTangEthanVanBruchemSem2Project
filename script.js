b9ddd4aea7da4d1d64204e66ab0e9eee57126c5a
Tom T <tomt.@Unknown.local>
2020-06-23 10:22:16 +0800

    balance update and more potions

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ var playery = 6;
 // skeletons are 2action 4damage 12hp 15xp
 
 // Enemy Bank
-var enemy1 = [3, 3, 1, 1, 3, 4, 'Green Slime', 3, 'g']
-var enemy2 = [4, 5, 2, 3, 5, 10, 'Blue Slime', 5, 'b']
-var enemy3 = [3, 5, 3, 5, 10, 25, 'Red Slime', 10,'r']
+var enemy1 = [3, 3, 2, 2, 5, 4, 'Green Slime', 5, 'g']
+var enemy2 = [4, 5, 3, 4, 10, 10, 'Blue Slime', 10, 'b']
+var enemy3 = [3, 5, 5, 6, 20, 25, 'Red Slime', 20,'r']
 var enemy4 = [5, 2, 2, 4, 12, 15, 'Skeleton', 12,'s']
 var chest = [7, 6, 0, 0, 1, 's', 'Chest', 1, 'c']
 var shop = [2, 2, 0, 0, 1, 's', 'Shop', 1, '$']
 var lock = [8, 4, 0, 0, 1, 's', 'Lock', 1, 'L']
-var boss1 = [6, 6, 4, 4, 100, 100, 'BOSS King Slime', 100, 'k']
+var boss1 = [6, 6, 6, 8, 200, 100, 'BOSS King Slime', 200, 'k']
 var placeholder = [0,0,0,0,0,0,'Placeholder',0,'z']
 // Enemies in play
 var currentEnemy1 = enemy1
@@ -180,11 +180,11 @@ function keypresschecker(e){
             attack(weapon4)
         }
     }
-    if (actualkey=='5') {
+    if (actualkey=='5' && hpPotionAmount != 0) {
         hpPotion()
         removehpPotion()
     }
-    if (actualkey=='6') {
+    if (actualkey=='6'&& mpPotionAmount != 0) {
         mpPotion()
         removempPotion()
     }
